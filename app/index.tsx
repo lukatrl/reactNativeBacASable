150c7c2086ff0d2103e8117cd2940aa01f044f49
lukatrl <lukatrouiller@gmail.com>
2024-12-22 03:08:26 +0100

    add component ThemeText.tsx & add constant Colors.ts

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedText } from "@/components/ThemedText";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/about">a propos</Link>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ThemedText variant="headline">Pokédex</ThemedText>  {/*Utilisation de la variante "headline" pour le text */}
+    </SafeAreaView>
   );
 }
 
 
 const styles = StyleSheet.create({
-  container: {backgroundColor: "#FF0000", padding: 24}
+  container: {backgroundColor: "#FF0000"}
 })
---
+++ components/ThemedText.tsx
@@ -0,0 +1,41 @@
+import {StyleSheet, Text, type TextProps} from "react-native";
+
+const styles = StyleSheet.create({ // Création de différentes polices d'écritures
+    body3: {
+        fontSize: 10,
+        lineHeight: 16,
+    },
+    headline: {
+        fontSize: 24,
+        lineHeight: 32,
+        fontWeight: "bold",
+    },
+    caption: {
+        fontSize: 8,
+        lineHeight: 12,
+    },
+    subtitle1: {
+        fontSize: 14,
+        lineHeight: 16,
+        fontWeight: "bold",
+    },
+    subtitle2: {
+        fontSize: 12,
+        lineHeight: 16,
+        fontWeight: "bold",
+    },
+    subtitle3: {
+        fontSize: 10,
+        lineHeight: 16,
+        fontWeight: "bold",
+    },
+})
+
+type Props = TextProps & {
+    variant?: keyof typeof styles,
+    color?: string
+}
+
+export function ThemedText ({variant, color, ...rest}: Props) {
+    return <Text style={styles[variant ?? 'body3']} {...rest}/>
+}
